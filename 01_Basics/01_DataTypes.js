b92f1bf0af233f620c2abf1ec479063e279ddc3f
tryingtobegoodperson <105357932+tryingtobegoodperson@users.noreply.github.com>
2024-09-11 12:14:04 +0000

    Basics of variables and datatypes done

---
+++ 01_Basics/01_DataTypes.js
@@ -0,0 +1,15 @@
+"use strict"// treat all JS code as newer version of JS
+
+//code readability must be High
+let name = "Sagar"
+let age = 33
+let isLoggedIn = true
+let state;
+
+console.log(typeof name)
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof null);
+
+
+
---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,17 @@
+const accountId = "4176"
+let accountEmail = "Sagar@in.com"
+var accountPassword = "xyz"
+accountCity = "Mumbai"
+accountEmail="Sagar.Tayde@gma.com"
+accountPassword = "bvc"
+
+// accountId="765"
+//Prefer not to use var 
+/*
+
+*/
+let accountState;
+
+
+console.log(accountState);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
